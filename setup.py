313c040df41087708d095869b8f9914579820b23
Jang Future <milktea0614@naver.com>
2023-08-31 20:26:30 +0900

    create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='future-logger',
+    version='0.0.1',
+    packages=['future-logger'],
+    url='https://github.com/milktea0614/future-logger',
+    license='',
+    author='future',
+    author_email='milktea0614@naver.com',
+    description='Custom logger for Jang, future',
+    keywords=['pypi deploy', 'future logger'],
+    python_requires='>=3',
+    classifiers =[
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ]
+)
